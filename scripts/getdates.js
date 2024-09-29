63bc6cc0c5058a2689b3631a9bd9eae9f43d7553
ReallyAmazing <michaelmacababbad05@gmail.com>
2024-09-29 21:15:16 +0800

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -8,10 +8,10 @@ const today = new Date();
 
 currentyear.innerHTML = `©️ <span class="highlight">${today.getFullYear()}</span> `;
 
-lastModified.innerHTML = `lastModified: <span class="highlight">${new Intl.DateTimeFormat(
+lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat(
 	"en-US",
 	{
-		dateStyle: "lastModified"
+		dateStyle: "short"
 	}
 ).format(today)}</span>`;
 
