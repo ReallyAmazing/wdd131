aa6be12b0d329718aa974364acf42f747f2bab19
ReallyAmazing <michaelmacababbad05@gmail.com>
2024-09-29 20:35:23 +0800

    lastmodified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,18 @@
 // select the DOM elements for output
 const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
 
 
 // use the date object
 const today = new Date();
 
 currentyear.innerHTML = `©️ <span class="highlight">${today.getFullYear()}</span> `;
+
+lastModified.innerHTML = `lastModified: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "lastModified"
+	}
+).format(today)}</span>`;
 
 
