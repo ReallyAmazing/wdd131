e4253798b705fe269dae65a9f4a2ddd58d852533
ReallyAmazing <michaelmacababbad05@gmail.com>
2024-10-09 06:37:52 +0800

    modify

--- scripts/temples.js
+++ scripts/temples.js
@@ -13,3 +13,14 @@ lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.Date
 		dateStyle: "short"
 	}
 ).format(today)}</span>`;
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
